e12c60debb5a2b1a9108ed292a26c40c40938a16
Fang <942585437@qq.com>
2016-12-01 11:47:53 +0800

    拦截器

--- src/cn/itcast/ssm/intercepter/HandlerIntercepter1.java
+++ src/cn/itcast/ssm/intercepter/HandlerIntercepter1.java
@@ -8,22 +8,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 其实是利用AOP机制
+ * springmvc的拦截器和struts2的不太一样 不是全局的 
+ * 1.针对某一个 handlerMapping进行拦截的，只有经过handlerMapping映射成功的handler才使用这个拦截器
  * 
+ * 2.springMvc 配置类似全局的拦截器：将配置的拦截器注入到每一个hanlderMapping中
  * @author Fangchao
  *
  */
 public class HandlerIntercepter1 implements HandlerInterceptor {
 
 	// 进入Handler方法之前执行
-	//用于身份认证和身份授权
+	//应用场景 :;用于身份认证和身份授权
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// TODO Auto-generated method stub
-		return false;
+		return false;//false 表示拦截 不向下执行
+		//return true;//放行
 	}
 
 	// 进入handler方法之后，返回modeandview之前执行
+	//应用场景从modeAndView出发：将公用的模型数据（比如菜单导航）在这里放入视图 也可以统一指定视图
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
@@ -31,6 +36,7 @@ public class HandlerIntercepter1 implements HandlerInterceptor {
 
 	}
 	//执行完Handler方法 之后
+	//应用场景 可以使用统一的异常处理 ，统一的日志处理
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
